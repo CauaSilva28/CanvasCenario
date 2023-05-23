58f3105340bebe6135c1e6589849ecebf77ab3ad
CauaSilva28 <caua.santos56@etec.sp.gov.br>
2023-05-23 10:45:47 -0300

    Fazendo cenário canvas

--- js/script.js
+++ js/script.js
@@ -16,19 +16,104 @@ function chao(){
 }
 
 function montanha(){
+    ctx.strokeStyle = '#9c4b0c';
+    ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.moveTo(0,500);
-    ctx.lineTo(150,350);
-    ctx.lineTo(350,600);
-    ctx.lineTo(450,500);
-    ctx.lineTo(550,600);
-    ctx.lineTo(750,400);
-    ctx.lineTo(850,500);
-    ctx.lineTo(1000,350);
-    ctx.lineTo(1150,550);
-    ctx.lineTo(1200,500);
+    ctx.lineTo(180,350);
+    ctx.lineTo(350,500);
+    ctx.lineTo(450,450);
+    ctx.lineTo(650,550);
+    ctx.lineTo(800,400);
+    ctx.lineTo(900,500);
+    ctx.lineTo(1050,350);
+    ctx.lineTo(1180,550);
+    ctx.lineTo(1200,530);
+    ctx.lineTo(1200,650);
+    ctx.lineTo(0,650);
+    ctx.lineTo(0,500);
+    ctx.fillStyle = '#ab510c';
+    ctx.fill();
     ctx.stroke();
 }
 
+function montanha2(){   
+    ctx.strokeStyle = '#963f23';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(0,530);
+    ctx.lineTo(200,600);
+    ctx.lineTo(300,500);
+    ctx.lineTo(500,600);
+    ctx.lineTo(650,450);
+    ctx.lineTo(800,600);
+    ctx.lineTo(950,500);
+    ctx.lineTo(1100,650);
+    ctx.lineTo(1180,600);
+    ctx.lineTo(1200,620);
+    ctx.lineTo(1200,700);
+    ctx.lineTo(0,700);
+    ctx.lineTo(0,530);
+    ctx.fillStyle = '#a1472a';
+    ctx.fill();
+    ctx.stroke();
+}
+
+function montanha3(){
+    ctx.strokeStyle = '#7d3c14';
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(0,350);
+    ctx.lineTo(150,450);
+    ctx.lineTo(400,250);
+    ctx.lineTo(600,400);
+    ctx.lineTo(750,300);
+    ctx.lineTo(1000,500);
+    ctx.lineTo(1200,300);
+    ctx.lineTo(1200,700);
+    ctx.lineTo(0,700);
+    ctx.lineTo(0,350);
+    ctx.fillStyle = "#8f471a";
+    ctx.fill();
+    ctx.stroke();
+}
+
+function sol(){
+    ctx.strokeStyle = '#ff9900';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(850,400,300,0,Math.PI*2,true);
+    ctx.fillStyle = '#ffb300';
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.strokeStyle = '#ffaa00';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(950,220,50,0,Math.PI*2,true);
+    ctx.fillStyle = "#ffae00";
+    ctx.fill();
+    ctx.stroke();
+    
+    ctx.strokeStyle = '#ffaa00';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(700,250,30,0,Math.PI*2,true);
+    ctx.fillStyle = "#ffae00";
+    ctx.fill();
+    ctx.stroke();
+
+    ctx.strokeStyle = '#ffaa00';
+    ctx.lineWidth = 10;
+    ctx.beginPath();
+    ctx.arc(900,400,20,0,Math.PI*2,true);
+    ctx.fillStyle = "#ffae00";
+    ctx.fill();
+    ctx.stroke();
+}
+
+sol();
+montanha3();
 montanha();
+montanha2();
 chao();
